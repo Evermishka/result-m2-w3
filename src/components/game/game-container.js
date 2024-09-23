b18c3e9cee264d9a9479997fca514ce64e5f19bb
Ermishina Evgeniya <evermishka@yandex.ru>
2024-09-23 19:26:19 +0300

    refactor game-container

--- src/components/game/game-container.js
+++ src/components/game/game-container.js
@@ -1,33 +1,43 @@
 import { GameLayout } from './game-layout';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { FIELD, WIN_PATTERNS } from '../../consts';
+
+const checkIsGameEnded = (currentField) =>
+	WIN_PATTERNS.some(
+		(el) =>
+			currentField[el[0]] !== '' &&
+			currentField[el[0]] === currentField[el[1]] &&
+			currentField[el[0]] === currentField[el[2]],
+	);
+
+const checkIsDraw = (currentField) => !currentField.some((ceil) => ceil === '');
 
 export const GameContainer = () => {
 	const [currentPlayer, setCurrentPlayer] = useState('X');
 	const [field, setField] = useState(FIELD);
-
-	const isDraw = !field.some((ceil) => ceil === '');
-
-	const isGameEnded = WIN_PATTERNS.some(
-		(el) =>
-			field[el[0]] !== '' &&
-			field[el[0]] === field[el[1]] &&
-			field[el[0]] === field[el[2]],
-	);
-
-	useEffect(() => {
-		if (!isGameEnded && !field.every((ceil) => ceil === '')) {
-			setCurrentPlayer((currentPlayer) => currentPlayer === 'X' ? 'O' : 'X');
-		}
-	}, [field, isGameEnded]);
+	const [isDraw, setIsDraw] = useState(false);
+	const [isGameEnded, setIsGameEnded] = useState(false);
 
 	const gameButtonClickHandler = (index) => {
-		setField(field.map((el, i) => (index === i ? currentPlayer : el)));
+		const currentField = field.map((el, i) => (index === i ? currentPlayer : el));
+		setField(currentField);
+
+		const isGameEndedTurn = checkIsGameEnded(currentField);
+		if (isGameEndedTurn) setIsGameEnded(true);
+
+		const isDrawTurn = checkIsDraw(currentField) && !isGameEndedTurn;
+		if (isDrawTurn) setIsDraw(true);
+
+		if (!isGameEndedTurn && !isDrawTurn) {
+			setCurrentPlayer((currentPlayer) => (currentPlayer === 'X' ? 'O' : 'X'));
+		};
 	};
 
 	const resetButtonClickHandler = () => {
 		setCurrentPlayer('X');
 		setField(FIELD);
+		setIsDraw(false);
+		setIsGameEnded(false);
 	};
 
 	return (
